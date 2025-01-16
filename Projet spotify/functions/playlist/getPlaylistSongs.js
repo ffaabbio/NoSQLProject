8fc6690e2f40386a6ef5367ba47b3c691e6272cd
Sandrine Ebelebe-Akouala <sandrine.ebelebe@gmail.com>
2025-01-16 12:49:20 +0100

    Delete Projet spotify/functions directory

--- Projet spotify/functions/playlist/getPlaylistSongs.js
+++
@@ -1,9 +0,0 @@
-const Playlist = require('../../database/models/playlist-model');
-
-async function getPlaylistSongs(playlistId) {
-    const playlist = await Playlist.findById(playlistId).populate('songs');
-    return playlist.songs;
-}
-
-
-module.exports = getPlaylistSongs;
